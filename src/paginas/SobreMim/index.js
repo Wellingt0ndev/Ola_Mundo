3a1724a0dc1054e764f86421ce98d70fdc759941
Wellington Ferreira da Silva <108769429+Wellingt0ndev@users.noreply.github.com>
2023-10-18 17:46:51 -0300

    Update Sobre mim

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -6,32 +6,24 @@ import fotoSobreMim from "assets/foto_sobre_mim.jpeg";
 export default function SobreMim() {
   return (
     <PostModelo fotoCapa={fotoCapa} titulo="Sobre mim">
-      <h3 className={styles.subtitulo}>Olá, eu sou o Antônio!</h3>
+      <h3 className={styles.subtitulo}>Olá, eu sou o Wellington!</h3>
 
       <img
         src={fotoSobreMim}
-        alt="Foto do Antônio Evaldo sorrindo"
+        alt="Foto do Wellington sorrindo"
         className={styles.fotoSobreMim}
       />
     <p className={styles.paragrafo}>
-                Oi, tudo bem? Eu sou instrutor de Front-end na Alura e estou feliz de te ver por aqui.
+    Oi, tudo bem? Eu sou estudante de Análise e Desenvolvimento de Sistemas e estou feliz de te ver por aqui.
             </p>
             <p className={styles.paragrafo}>
-                Minha história com programação começou no Instituto Federal do Piauí (IFPI), quando fiz o ensino médio integrado ao curso de Informática. Eu aprendi lógica de programação e o básico de várias linguagens, como PHP, Python, Java, mas sem aprofundar muito em cada uma delas. Eu gostava muito de estudar programação, mas na época não fazia ideia de que trabalharia com isso hoje.
+                Meu amor pela programação começou cedo, mas devido aos contratempos da vida, não entrei para a área de primeira. Sou formado em Logística e tenho MBA em Gestão de Pessoas, trabalhei aproximadamente 10 anos nessa área, mas sentia que não era o que eu realmente queria para a minha carreira. 
             </p>
             <p className={styles.paragrafo}>
-                No ensino superior, escolhi cursar Engenharia Elétrica na Universidade Federal do Piauí (UFPI). Lá eu consegui entrar no PET (Programa de Educação Tutorial), um grupo acadêmico que contribui de diversas formas para o curso. Lá eu tive a oportunidade de estudar desenvolvimento web para ser um dos mantenedores do site deles.
-            </p>
-            <p className={styles.paragrafo}>
-                Com isso tive minha experiência de dev mais próxima da realidade, com prazos de implementação para o site, e aprendi muito enquanto codificava.
-            </p>
-            <p className={styles.paragrafo}>
-                E foi enquanto eu estava no PET que decidi procurar um estágio em desenvolvimento web. Eu encontrei uma vaga na Alura para o Scuba Team e percebi que ela aliava duas paixões minhas: programação e educação. Para minha felicidade, fui escolhido para fazer parte do time.
+               Comecei a fazer Análise e Desenvolvimento de sistemas 1 mês depois de ter completado 31 anos de idade. Eu aprendi lógica de programação e o básico de várias linguagens, como C#, JavaScript e Java, onde me aprofundei um pouco mais. Eu gosto muito de estudar programação, meu objetivo hoje é conseguir um estágio para me aperfeiçoar mais e para ter minha experiência de dev mais próxima da realidade.
+            </p>                   
 
-            </p>
-            <p className={styles.paragrafo}>
-                Desde então, tem sido aprenas aprendizados atrás de aprendizados. A Alura é uma escola não só para seus alunos e alunas, mas também para os colaboradores e colaboradoras. Hoje sou muito feliz de ter a oportunidade de trazer esses conteúdos ricos e encantadores para você. Espero que aprenda bastante!
-            </p>
+
         </PostModelo>
     )
 }
